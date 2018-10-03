f0828b63042b07cf7c624e79bd5750005439406e
Tim Winters <twinters@wpi.edu>
2018-10-03 12:11:53 -0400

    Initial Commit

---
+++ FrostedGlass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FrostedGlass
+//
+//  Created by Tim Winters on 10/3/18.
+//  Copyright © 2018 Tim Winters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
